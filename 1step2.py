2f28a3cca646abb2fe0590a14d07e8db28157135
yihyun-kim1 <93534188+yihyun-kim1@users.noreply.github.com>
2021-11-24 17:08:56 +0900

    11/24

--- 1step2.py
+++ 1step2.py
@@ -60,7 +60,7 @@ def press2() :
 button = Button(window, text='Hi Veginner! 이번 단계의 요리 공개!', command=press1)
 button.pack(side=TOP)
 
-photo = PhotoImage(file="mosaic/가지_moza.png")
+photo = PhotoImage(file="mosaic/고등어조림01.png")
 btn = Button(window, width=200, height=200, image=photo, command=press2)
 window.mainloop()
 
@@ -72,7 +72,7 @@ window = Tk()
 window.geometry("1000x1000")
 window.title("재료 고르기")
 
-#아래 함수들과 버튼 작업은 단순 반복문의 경우와 다름 - 클래스 구현 시에 이 부분을 참고해서 구현해야함을 염두에 두어야 합니다
+#아래 함수들과 버튼 작업은 단순 반복문의 경우와 다름 - 클래스 구현 시에 이 부분을 참고해서 구현해야 함을 염두에 두어야 합니다
 def ingredient1() :
     ibtn = Button(window, text="고추를 어떻게 요리하시겠습니까?", command=press2)
     ibtn.grid(row=1, column=0)
@@ -80,11 +80,11 @@ def ingredient1() :
     #EX. 썰기/볶기 등
 
 def ingredient2() :
-    ibtn = Button(window, text="달걀을 어떻게 요리하시겠습니까?", command=press2)
+    ibtn = Button(window, text="생수를 어떻게 사용하시겠습니까?", command=press2)
     ibtn.grid(row=1, column=1)
 
 def ingredient3() :
-    ibtn = Button(window, text="감자를 어떻게 요리하시겠습니까?", command=press2)
+    ibtn = Button(window, text="무를 어떻게 요리하시겠습니까?", command=press2)
     ibtn.grid(row=1, column=2)
 
 def ingredient4() :
@@ -99,11 +99,11 @@ photo1 = PhotoImage(file="mosaic/고추2_moza.png")
 btn1 = Button(window, width=200, height=200, image=photo1, command=ingredient1)
 btn1.grid(row=0, column=0)
 
-photo2 = PhotoImage(file="mosaic/달걀_moza.png")
+photo2 = PhotoImage(file="mosaic/물_moza.png")
 btn2 = Button(window, width=200, height=200, image=photo2, command=ingredient2)
 btn2.grid(row=0, column=1)
 
-photo3 = PhotoImage(file="mosaic/감자_moza.png")
+photo3 = PhotoImage(file="mosaic/무_moza.png")
 btn3 = Button(window, width=200, height=200, image=photo3, command=ingredient3)
 btn3.grid(row=0, column=2)
 
@@ -125,7 +125,6 @@ def todo1() :
     butt1.destroy()
     ibutt = Button(window, text="다지기 선택!", command=press2)
     ibutt.grid(row=3, column=3)
-    ibutt.destroy()
     butt = Button(window, text="다지기 완료!", command=press2)
     butt.grid(row=1, column=3)
 
@@ -133,7 +132,6 @@ def todo2() :
     butt2.destroy()
     ibutt = Button(window, text="볶기 선택!", command=press2)
     ibutt.grid(row=3, column=3)
-    ibutt.destroy()
     butt = Button(window, text="볶기 완료!", command=press2)
     butt.grid(row=1, column=3)
 
